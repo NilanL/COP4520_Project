889766bda87c0ae784b44f8ba24fce46f0f08c59
NilanL <54152496+NilanL@users.noreply.github.com>
2023-03-05 19:32:01 -0500

    Update CustomCollections.java

--- CustomCollections.java
+++ CustomCollections.java
@@ -2,24 +2,25 @@ import java.util.*;
 
 public class CustomCollections<T extends Comparable<T>>
 {
-    // Assuming system has 4 logical processors
     private static final int MAX_THREADS = 4;
-       
-    // Custom Thread class with constructors
+    
     private static class SortThreads<T extends Comparable<T>> extends Thread
     {
-        SortThreads(T[] array, int begin, int end){
-            super(()->{
-                CustomCollections.mergeSort(array, begin, end);
-            });
+        public SortThreads(T[] array, int begin, int end)
+        {
+            super (
+                () -> {
+                    CustomCollections.mergeSort(array, begin, end);
+                }
+            );
+
             this.start();
         }
     }
      
     // Perform Threaded merge sort
-    public static <T extends Comparable<? super T>> void sort(T [] array)
+    public static <T extends Comparable<T>> void sort(T [] array)
     {
-        //long time = System.currentTimeMillis();
         final int length = array.length;
 
         boolean exact = length % MAX_THREADS == 0;
@@ -32,7 +33,7 @@ public class CustomCollections<T extends Comparable<T>>
             int beg = i;
             int remain = length - i;
             int end = remain < lengthPerThread ? i + (remain - 1) : i + (lengthPerThread - 1); 
-            final SortThreads<?> t = new SortThreads(array, beg, end);
+            final SortThreads<?> t = new SortThreads<>(array, beg, end);
 
             threads.add(t);
         }
@@ -56,12 +57,10 @@ public class CustomCollections<T extends Comparable<T>>
             int end = remain < lengthPerThread ? i + (remain - 1) : i + (lengthPerThread - 1);
             merge(array, 0, mid, end);
         }
-        //time = System.currentTimeMillis() - time;
-        //System.out.println("Time spent for custom multi-threaded recursive merge_sort(): "+ time+ "ms");
     }
  
     // Typical recursive merge sort
-    public static <T extends Comparable<? super T>> void mergeSort(T[] array, int begin, int end)
+    public static <T extends Comparable<T>> void mergeSort(T[] array, int begin, int end)
     {
         if (begin < end)
         {
@@ -73,16 +72,13 @@ public class CustomCollections<T extends Comparable<T>>
     }
     
     @SuppressWarnings("unchecked")
-    //Typical 2-way merge
-    public static <T extends Comparable<? super T>> void merge(T[] array, int begin, int mid, int end)
+    public static <T extends Comparable<T>> void merge(T[] array, int begin, int mid, int end)
     {
         Object[] temp = new Object [(end - begin) + 1];
          
         int i = begin, j = mid + 1;
         int k = 0;
  
-        // Add elements from first half or second half based on whichever is lower,
-        // do until one of the list is exhausted and no more direct one-to-one comparison could be made
         while (i <= mid && j <= end){
             if (array[i].compareTo(array[j]) <= 0)
             {
@@ -97,7 +93,6 @@ public class CustomCollections<T extends Comparable<T>>
             k += 1;
         }
  
-        // Add remaining elements to temp array from first half that are left over
         while (i <= mid)
         {
             temp[k] = array[i];
@@ -105,18 +100,12 @@ public class CustomCollections<T extends Comparable<T>>
             k += 1;
         }
         
-        // Add remaining elements to temp array from second half that are left over
         while (j <= end)
         {
             temp[k] = array[j];
             j += 1; 
             k += 1;
         }
-
-        // for (i = begin, k = 0; i <= end; i++,k++)
-        // {
-        //     array[i] = temp[k];
-        // }
         
         for (i = begin; i <= end; i++)
         {
