ff11a366da9410a169b2a3ceb104a5caadbb35ba
NilanL <54152496+NilanL@users.noreply.github.com>
2023-04-15 17:58:43 -0400

    Separated sorting methods

--- CustomCollections.java
+++ CustomCollections.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class CustomCollections<T extends Comparable<T>>
 {
     private static int MAX_THREADS = 4;
-    
+    private static int length;
+
     private static class SortThreads<T extends Comparable<T>> extends Thread
     {
         public SortThreads(T[] array, int begin, int end)
@@ -24,21 +25,9 @@ public class CustomCollections<T extends Comparable<T>>
     // Perform Threaded merge sort
     public static <T extends Comparable<T>> void sort(T [] array)
     {
-        final int length = array.length;
+        length = array.length;
 
-        boolean exact = length % MAX_THREADS == 0;
-        int lengthPerThread = 0;
-        
-        if (exact)
-        {
-            lengthPerThread = length / MAX_THREADS;
-        }
-        else
-        {
-            lengthPerThread = length / (MAX_THREADS - 1);
-        }
-        
-        lengthPerThread = lengthPerThread < MAX_THREADS ? MAX_THREADS : lengthPerThread;
+        int lengthPerThread = getThreadPartition(array);
 
         final ArrayList<SortThreads<?>> threads = new ArrayList<>();
 
@@ -74,7 +63,31 @@ public class CustomCollections<T extends Comparable<T>>
             }
         }
 
-        for(int i = 0; i < length; i += lengthPerThread)
+        mergePartitions(array, lengthPerThread);
+    }
+
+    // Determines how to partition array for N number of threads
+    public static <T extends Comparable<T>> int getThreadPartition(T[] array)
+    {
+        boolean exact = length % MAX_THREADS == 0;
+        int lengthPerThread = 0;
+        
+        if (exact)
+        {
+            lengthPerThread = length / MAX_THREADS;
+        }
+        else
+        {
+            lengthPerThread = length / (MAX_THREADS - 1);
+        }
+        
+        return lengthPerThread < MAX_THREADS ? MAX_THREADS : lengthPerThread;
+    }
+
+    // Merges sorted partitioned sub arrays
+    public static <T extends Comparable<T>> void mergePartitions(T[] array, int lengthPerThread)
+    {
+        for (int i = 0; i < length; i += lengthPerThread)
         {
             int mid = i == 0 ? 0 : i - 1;
             int remain = length - i;
@@ -82,6 +95,7 @@ public class CustomCollections<T extends Comparable<T>>
             merge(array, 0, mid, end);
         }
     }
+
 
     // Typical recursive merge sort
     public static <T extends Comparable<T>> void mergeSort(T[] array, int begin, int end)
@@ -103,7 +117,7 @@ public class CustomCollections<T extends Comparable<T>>
         {
             T key = arr[i];
             int j = i - 1;
-            
+
             while (j >= 0 && arr[j].compareTo(key) > 0) 
             {
                 arr[j + 1] = arr[j];
